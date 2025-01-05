91b76b951d1aa11bb205c1e8075c9643af33e336
Hanspeter Gosteli <hanspeter.gosteli@gmail.com>
2025-01-05 08:07:26 +0100

    remove another comment

--- systemctl_mqtt/_dbus/control_manager.py
+++ systemctl_mqtt/_dbus/control_manager.py
@@ -15,9 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# Gosteli
-# This file allow for a MVP of --control-system-unit [unit_name]
-
 import jeepney
 import jeepney.io.blocking
 
