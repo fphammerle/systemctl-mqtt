3068652fe5d039118a247460a7a9419cf5983e5e
Hanspeter Gosteli <hanspeter.gosteli@gmail.com>
2025-01-06 18:40:30 +0100

    lint

--- systemctl_mqtt/__init__.py
+++ systemctl_mqtt/__init__.py
@@ -266,12 +266,13 @@ class _MQTTActionSchedulePoweroff(_MQTTAction):
 
 
 class _MQTTActionRestartUnit(_MQTTAction):
-    # pylint: disable=too-few-public-methods
+    # pylint: disable=protected-access,too-few-public-methods
     def __init__(self, unit_name: str):
         self._unit_name = unit_name
     def trigger(self, state: _State) -> None:
-        systemctl_mqtt._dbus.service_manager.restart_unit(unit_name=self._unit_name)
-
+        systemctl_mqtt._dbus.service_manager.restart_unit(
+            unit_name=self._unit_name
+        )
 
 class _MQTTActionLockAllSessions(_MQTTAction):
     # pylint: disable=too-few-public-methods
