23dc2d38ef5365dc71b419d1a92a7283c21be56e
NMS Indium <nathan@indium.zln.nmsni.co.uk>
2025-03-31 14:45:30 +0100

    ready for merge

--- systemctl_mqtt/__init__.py
+++ systemctl_mqtt/__init__.py
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-print("nathan's version 3")
-
 import abc
 import argparse
 import asyncio
