d9c33da70100f8d77d5426e9c0bc1b2918caefb7
Florian Eitel <feitel@indeedgeek.de>
2025-09-07 19:06:22 +0200

    Update systemctl_mqtt/__init__.py
    Co-authored-by: Fabian Peter Hammerle <t2967wm9@4wrd.cc>

--- systemctl_mqtt/__init__.py
+++ systemctl_mqtt/__init__.py
@@ -346,18 +346,11 @@ async def _mqtt_message_loop(*, state: _State, mqtt_client: aiomqtt.Client) -> N
         action_by_topic[topic] = action
 
     for unit_name in state.controlled_system_unit_names:
-        topic = state.mqtt_topic_prefix + "/unit/system/" + unit_name + "/start"
+for topic_suffix, action_class in [('start', _MQTTActionStartUnit), ('stop', _MQTTActionStopUnit)]:
+        topic = state.mqtt_topic_prefix + "/unit/system/" + unit_name + "/" + topic_suffix
         _LOGGER.info("subscribing to %s", topic)
         await mqtt_client.subscribe(topic)
-        action = _MQTTActionStartUnit(unit_name=unit_name)
-        action_by_topic[topic] = action
-
-        topic = state.mqtt_topic_prefix + "/unit/system/" + unit_name + "/stop"
-        _LOGGER.info("subscribing to %s", topic)
-        await mqtt_client.subscribe(topic)
-        action = _MQTTActionStopUnit(unit_name=unit_name)
-        action_by_topic[topic] = action
-
+        action_by_topic[topic] = action_class(unit_name=unit_name)
         topic = state.mqtt_topic_prefix + "/unit/system/" + unit_name + "/restart"
         _LOGGER.info("subscribing to %s", topic)
         await mqtt_client.subscribe(topic)
